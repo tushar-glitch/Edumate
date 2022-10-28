87415c5585632dbeadde0e7889719d1e5a4c0581
sanika <sanika2110118@akgec.ac.in>
2022-10-28 00:45:22 -0700

    reset password validation implemented via Apis

--- src/components/resetPwd/resetPwd.jsx
+++ src/components/resetPwd/resetPwd.jsx
@@ -1,23 +1,61 @@
 import React, { useState, useEffect } from "react";
 import Background from "../Background/Background";
 import ResetImg from "./resPwdImg";
+import {Link} from "react-router-dom";
 import "./resetPwd.css";
+import axios from "axios";
 
 function ResetPwd() {
   const [pass, setPass] = useState("");
   function handlepass(e) {
     setPass(e.target.value);
   }
+  const [Cpass, setCPass] = useState("");
+  function handleCfmPass(e) {
+    setCPass(e.target.value);
+  }
+  const [isPass, setIsPass] = useState(false);
+  const [isCPass, setIsCPass] = useState(false);
   const rightpass =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
   useEffect(() => {
     if (rightpass.test(pass)) {
       document.getElementById("wrongpass").style.display = "none";
+setIsPass(true)
       console.log("true");
     } else if (pass) {
       document.getElementById("wrongpass").style.display = "block";
     }
   }, [pass]);
+  useEffect(() => {
+    if (rightpass.test(pass)) {
+      document.getElementById("wrongpass").style.display = "none";
+      setIsCPass(true);
+      console.log("true");
+    } else if (pass) {
+      document.getElementById("wrongpass").style.display = "block";
+    }
+  }, [Cpass]);
+const [rdtLogin, setRdtLogin] = useState(false);
+  function rstPassword(){
+    var email = localStorage.getItem("email")
+    console.log(email);
+    var otp = localStorage.getItem("otp");
+    var data={email,otp,password:pass,confirmpassword:Cpass};
+      if (isPass && isCPass ) {
+        axios
+          .post("https://erp-edumate.herokuapp.com/api/user/changepassword/",data)
+          .then((res) => {
+            console.log(res.data);
+            setRdtLogin(true);
+           alert("Password changed");
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("Password is not chamged, try it again");
+          });
+      }
+  }
   return (
     <>
       <Background />
@@ -36,8 +74,10 @@ function ResetPwd() {
         type="text"
         id="confirm-pass-input"
         placeholder="Confirm Password"
+        onChange={handleCfmPass}
       />
-      <button id="btn-reset">RESET PASSWORD</button>
+      {rdtLogin?(<Link to="/"> <button id="btn-reset" onClick={rstPassword}>RESET PASSWORD</button></Link>):(<Link to="/"> <button id="btn-reset" onClick={rstPassword}>RESET PASSWORD</button></Link>)}
+      {/* <button id="btn-reset" onClick={rstPassword}>RESET PASSWORD</button> */}
       <ResetImg />
     </>
   );
