d83bff5320bbe946ceb543e8309aea5b585d656a
Tushar <tusharc20001@gmail.com>
2022-11-02 22:28:19 +0530

    Added Student profile section

--- src/components/Student-profile/student.jsx
+++ src/components/Student-profile/student.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import './student.css'
 import avatar from '../Assests/Images/avatar.png'
+import Navbar from '../Navbar/Navbar'
 const student = () => {
     return (
         <>
+            <Navbar/>
             <h1 id='dash'>Dashboard &gt; My Profile</h1>
             <div id="background">
-                <div id="avatar"><img src={avatar}/></div>
+                <div id="avatar">
+                    <img src={avatar} />
+                    <span id='dis-name'>TUSHAR CHAUHAN </span><br />
+                    Mon 3 SEPT 2001 <br />
+                    B.TECH 2ND YEAR <br />
+                    CSE
+                </div>
                 <div id="heading1">Student Profile</div>
                 <div id="personalinfo">Personal Info</div>
-                <div id="stu-name">Student's Name Tushar Chauhan</div>
-                <div id="roll">Roll No. 2100xxxxxxxxx</div>
-                <div id="adm-no">Admission No. 21xxxxx</div>
+                <div id="stu-name">Student's Name <span className='space1'>Tushar Chauhan</span></div>
+                <div id="roll">Roll No. <span className='space2'>2100xxxxxxxxx</span></div>
+                <div id="adm-no">Admission No. <span className='space3'>21xxxxx</span></div>
                 <div id="bl-gr">Blood Group</div>
                 <div id="email">Email</div>
                 <div id="heading2">Contact Details</div>
